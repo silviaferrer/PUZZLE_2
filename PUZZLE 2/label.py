592e6b2182d2d1595c718333fa4c0f27d7233773
silviaferrer <62883007+silviaferrer@users.noreply.github.com>
2021-10-30 13:57:31 +0200

    Add files via upload

--- PUZZLE 2/label.py
+++ PUZZLE 2/label.py
@@ -5,6 +5,7 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
+from gi.repository import GLib
 
 
 class LabelWindow(Gtk.Window):
@@ -15,48 +16,51 @@ class LabelWindow(Gtk.Window):
         self.box = Gtk.Box(spacing=6)
         self.box.set_homogeneous(False)
         
-        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing=6)
         
         self.add(self.box)
         
-        self.evbox = Gtk.EventBox()
-        self.evbox.override_background_color(0, Gdk.RGBA(0,0,8,1))
-
         self.box.pack_start(self.box, True, True, 0)
 
-        self.label = Gtk.Label('<span foreground="white" size="x-large">Please login with your university card</span>')
+        self.label = Gtk.Label(label="Please login with your university card")
         self.label.set_justify(Gtk.Justification.CENTER)
-        self.label.set_use_markup(True)
-        self.label.set_name("Inicial_label")
         self.label.set_size_request(400,100)
         self.box.pack_start(self.label, True, True, 0)
-        
-        self.evbox.add(self.label)
         
         self.button = Gtk.Button(label="Clear")
         self.button.connect("clicked", self.clicked)
         self.box.pack_start(self.button, True, True, 0)
         
-        thread = threading.Thread(target=self.read_uid)
-        thread.setDaemon(True)
-        thread.start()
+        self.thread = threading.Thread(target=self.read_uid)
+        self.thread.setDaemon(True)
+        self.thread.start()
+        
+        self.styles = Gtk.CssProvider()
+        self.styles.load_from_path("style_prov/estilos.css")
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), self.styles, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        self.label.get_style_context().add_class("start")        
         
         self.connect("destroy", Gtk.main_quit)
         self.show_all()
         Gtk.main()
     
     def clicked (self, widget):
-        self.label.set_label('<span foreground="white" size="x-large">Please login with your university card</span>')
-        self.evbox.override_background_color(0, Gdk.RGBA(0,0,8,1))
+        self.label.set_text("Please login with your university card")
+        self.label.get_style_context().remove_class("uid_screen")
+        self.label.get_style_context().add_class("start")
         
-        thread = threading.Thread(target=self.read_uid)
-        thread.start()
+        self.thread = threading.Thread(target=self.read_uid)
+        self.thread.start()
         
     def read_uid (self):
         p1 = code.puzzle_1()
-        uid = p1.read_card()
+        self.uid = p1.read_card()
+        GLib.idle_add(self.update)
         
-        self.label.set_label('<span foreground="white" size="x-large">uid:'+uid+'</span>')
-        self.evbox.override_background_color(0, Gdk.RGBA(8,0,0,1))
+        
+    def update (self):
+        self.label.get_style_context().remove_class("start")
+        self.label.get_style_context().add_class("uid_screen")
+        self.label.set_text("uid: "+self.uid)
         
 w = LabelWindow()       
